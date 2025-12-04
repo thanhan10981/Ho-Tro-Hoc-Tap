48cd2ef46e96a5ee87647e1434063cd93765bb4b
thanhan10981 <152773355+thanhan10981@users.noreply.github.com>
2025-12-05 00:53:44 +0700

    update

---
+++ ho_tro_hoc_tap/src/presentation/layouts/MainLayout.tsx
@@ -0,0 +1,13 @@
+import FloatingButtons from "../components/FloatingButtons";
+import Navbar from "../components/Navbar";
+
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Navbar />
+    
+      <div className="pt-24 px-10">{children}</div>
+      <FloatingButtons />
+    </div>
+  );
+}
